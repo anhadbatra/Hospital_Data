b57b07bd3945ead75461d9e2bba35823e2df2e2a
itisanhad <anhadpreetadoresapples@helpusdefend.com>
2025-01-15 01:20:12 -0500

    redshift update

--- functions.py
+++ functions.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from sqlalchemy import create_engine
+import sqlalchemy as sa
+from sqlalchemy.engine.url import URL
 import os 
 AIRFLOW_HOME = os.environ.get('AIRFLOW_HOME')
 
@@ -7,10 +8,44 @@ def extract_data():
     df = pd.read_csv(AIRFLOW_HOME + 'dags/odhf_bdoes_v1.csv',encoding='unicode_escape')
     df.dropna(subset=['street_name','source_format_str_address','CSDuid','latitude','longitude'], inplace=True)
     df['index'] = range(len(df))
-    return df
+    url = URL.create(
+    drivername='redshift+redshift_connector', 
+    host='default-workgroup.058264275627.us-east-1.redshift-serverless.amazonaws.com', 
+    port=5439, 
+    database='hospital_data', # Amazon Redshift database
+    username= os.environ.get('redshift_user'), # Amazon Redshift username
+    password= os.environ.get('redshift_password') # Amazon Redshift password
+    )
+    engine = sa.create_engine(url)
+    table_name = 'hospital_data'
+    create_table_query = f"""
+    CREATE TABLE IF NOT EXISTS {table_name} (
+    index INT,
+    facility_name VARCHAR(255),
+    source_facility_type VARCHAR(100),
+    odhf_facility_type VARCHAR(100),
+    provider VARCHAR(255),
+    unit VARCHAR(50),
+    street_no VARCHAR(50),
+    street_name VARCHAR(255),
+    postal_code VARCHAR(10),
+    city VARCHAR(100),
+    province VARCHAR(2),
+    source_format_str_address VARCHAR(255),
+    CSDname VARCHAR(100),
+    CSDuid INT,
+    Pruid INT,
+    latitude FLOAT,
+    longitude FLOAT
+);
+"""
+
+    with  engine.connect() as connection:
+        connection.execute(f"TRUNCATE TABLE {table_name}")
+        connection.execute(create_table_query)
+    df.to_sql(table_name, engine, if_exists='append', index=False)
 
 
 
-def load_data(df):
-    engine = create_engine('postgresql:import_csv//@localhost:5432/staging_hospital_data')
-    df.to_sql("filtered_data_set",con=engine,if_exists='replace')
+def analyze_data():
+    print("Data has been analyzed")
--- hospital.py
+++ hospital.py
@@ -1,23 +1,18 @@
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
-from functions import extract_data,transform_data,load_data
+from functions import extract_data,analyze_data
 
 with DAG("import_hospital_data",start_date=datetime(2021,1,1),schedule_interval="@daily",catchup=False) as dag: 
     extract = PythonOperator(
-        task_id="extract",
+        task_id="extract_and_load",
         python_callable=extract_data,
     )
-
-    transform = PythonOperator(
-        task_id="transform",
-        python_callable=transform_data,
+    analyse = PythonOperator(
+        task_id="analyse",
+        python_callable=analyze_data,
     )
 
-    load = PythonOperator(
-        task_id="load",
-        python_callable=load_data,
-    )
 
     # Set task dependencies
-    extract >> transform >> load
+    extract >> analyse
